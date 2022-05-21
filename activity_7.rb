b005f9d83a01aca4ea060760c191896b01e7328f
nazamateo <nazamateo@gmail.com>
2022-05-21 11:54:26 +0800

    Hard coded solution

--- activity_7.rb
+++ activity_7.rb
@@ -1,14 +1,15 @@
-str = "Pneumonoultramicroscopicsilicovolcanoconiosis"
+str = "aabc"
 
 def check_repeated_char (str) 
-    strarray = str.split("")
-    strarray.each_with_index do |element|
-        strarray.each do |char|
-            if element == char
-                return p "String is not an Isogram"
+    strarray = str.downcase.split("")
+    strarray.each_with_index do |element, i|
+        strarray.each_with_index do |char, i2|
+            if i != i2
+                return p "not an isogram" if element == char
             end
         end
-    end 
+    end
+    p "isogram"
 end
 
 check_repeated_char(str)
