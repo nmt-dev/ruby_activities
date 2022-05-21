8311fcb9d3e8856a251abf248f11ce5946249bb1
nazamateo <nazamateo@gmail.com>
2022-05-21 10:14:09 +0800

    Revised solution using 2 loops

--- activity_7.rb
+++ activity_7.rb
@@ -1,10 +1,18 @@
 str = "Pneumonoultramicroscopicsilicovolcanoconiosis"
 
-def check_repeated_char (str)
+def check_repeated_char (str) 
     strarray = str.split("")
-    uniqchars = strarray.uniq
-    print "Given string: #{str} \n"
-    p strarray.length == uniqchars.length ? "#{str} is an Isogram" : "#{str} is not an Isogram"
+    strarray.each_with_index do |element|
+        strarray.each do |char|
+            if element == char
+                return p "String is not an Isogram"
+            end
+        end
+    end 
+    # strarray = str.split("")
+    # uniqchars = strarray.uniq
+    # print "Given string: #{str} \n"
+    # p strarray.length == uniqchars.length ? "#{str} is an Isogram" : "#{str} is not an Isogram"
 end
 
 check_repeated_char(str)
